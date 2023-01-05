c2180f74325ab4bb61c6ba4e1c2c6766070daa35
ZJW-92 <zjwei0201@gmail.com>
2023-01-05 07:01:33 +0100

    update app.js and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom'; 
 import {Box} from '@mui/material'; 
-
-
-
 import './App.css' ; 
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
